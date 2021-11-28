2b48527c93f4a4a41489d9c2171e6eab8fecd432
LisaHackett97 <lisa-hackett@hotmail.com>
2021-11-28 20:14:42 +0000

    Add cards to services page to hold content

--- services/models.py
+++ services/models.py
@@ -9,5 +9,6 @@ class Service(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(null=True, blank=True)
 
+
     def __str__(self):
         return self.name
