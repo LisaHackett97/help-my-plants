ae60bf04d531d4ea52e6ffbcb185d77b93fc8c93
LisaHackett97 <lisa-hackett@hotmail.com>
2021-11-28 17:42:56 +0000

    Add products views and urls

--- services/views.py
+++ services/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Service
+
 
 # Create your views here.
+
+def all_services(request):
+    """ A view to show all services """
+
+    services = Service.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'services/services.html', context)
+
