cd5e162a87be1dcbcacb4baff9f95ee7d6a8207c
LisaHackett97 <lisa-hackett@hotmail.com>
2021-11-29 08:22:41 +0000

    Add view and basic template for add new product

--- services/views.py
+++ services/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Service
-
+from .forms import ServiceForm
 
 # Create your views here.
+
 
 def all_services(request):
     """ A view to show all services """
@@ -15,6 +16,7 @@ def all_services(request):
 
     return render(request, 'services/services.html', context)
 
+
 def service_detail(request, service_id):
     """ a view to display individual service details """
     service = get_object_or_404(Service, pk=service_id)
@@ -24,3 +26,14 @@ def service_detail(request, service_id):
     }
 
     return render(request, 'services/service_detail.html', context)
+
+
+def add_service(request):
+    """ Add a service to the site """
+    form = ServiceForm()
+    template = 'services/add_service.html'
+    context = {
+        'form': form
+    }
+
+    return render(request, 'services/add_service', context)
