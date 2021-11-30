592f8a7140281a860a8ccd593470a0a403c78413
LisaHackett97 <lisa-hackett@hotmail.com>
2021-11-30 08:24:41 +0000

    Add Order to admin and register app, migrations done

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Order, OrderItem
 
-# Register your models here.
+class OrderItemAdminInline(admin.TabularInline):
+    model = OrderItem
+    readonly_fields = ('item_total',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderItemAdminInline,)
+
+    readonly_fields = ('order_number', 'order_total', 'date')
+
+    fields = ('order_number',
+              'customer_name',
+              'email',
+              'phone_number',
+              'time_slot',
+              'order_total',
+              'date'
+              )
+
+    list_display = ('order_number', 'customer_name', 'time_slot', 'order_total',)
+
+    ordering = ('-date',)
+
+admin.site.register(Order, OrderAdmin)
+
