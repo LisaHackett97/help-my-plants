7082909d1553e6760a068a7c7a175daa109c1dbe
LisaHackett97 <lisa-hackett@hotmail.com>
2021-12-01 08:05:48 +0000

    Add checkout forms.py

---
+++ checkout/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from .models import Order
+
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ('customer_name', 'email', 'phone_number',
+                  'time_slot',)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders 
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'customer_name': 'Full Name',
+            'email': 'Email Address',
+            'phone_number': 'Phone Number',
+            'time_slot': 'Time Chosen',
+            }
