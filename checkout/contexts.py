7551b24bbb7e1b37013950c7060ee3e4d76157ee
LisaHackett97 <lisa-hackett@hotmail.com>
2021-12-02 17:41:16 +0000

    Update context, views and templates to render add to cart

--- checkout/contexts.py
+++ checkout/contexts.py
@@ -1,8 +1,22 @@
+from django.conf import settings
+from django.shortcuts import get_object_or_404
+from services.models import Service
+
 def cart_contents(request):
 
     cart_items = []
     total = 0
     service_count = 0
+    cart = request.session.get('cart', {})
+
+    for item_id, quantity in cart.items():
+        service = get_object_or_404(Service, pk=item_id)
+        service_count = quantity
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'service': service
+        })
 
 
     context = {
--- checkout/views.py
+++ checkout/views.py
@@ -20,11 +20,17 @@ def checkout(request):
     return render(request, template, context)
 
 
+
 def add_to_cart(request, item_id):
     """ Add a service to the Cart """
-    service = get_object_or_404(Service, pk=item_id)
+    quantity = request.POST.get('quantity')
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
+
+    if item_id in list(cart.keys()):
+        cart[item_id] += quantity
+    else:
+        cart[item_id] = quantity
 
     request.session['cart'] = cart
     print(request.session['cart'])
