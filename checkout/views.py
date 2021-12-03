d6955bf44c60d29ed3310182201f023d42c996d8
LisaHackett97 <lisa-hackett@hotmail.com>
2021-12-03 20:17:34 +0000

    Finish addings toasts

--- checkout/views.py
+++ checkout/views.py
@@ -12,14 +12,14 @@ def view_cart(request):
 
 
 def add_to_cart(request, item_id):
-    """ 
-    Add a service to the Cart 
+    """
+    Add a service to the Cart
     User can only add one item for each service to cart
     They are redirected to cart
     """
-    service = Service.objects.get(pk=item_id)   
+    service = get_object_or_404(Service, pk=item_id)
     cart = request.session.get('cart', {})
-    
+
     if item_id in cart:
         messages.info(request, f'{service.name} already in your cart')
     else:
@@ -32,13 +32,12 @@ def add_to_cart(request, item_id):
 def remove_from_cart(request, item_id):
     """ Remove an item from the cart """
     cart = request.session.get('cart', {})
-    service = Service.objects.get(pk=item_id) 
+    service = get_object_or_404(Service, pk=item_id)
     cart.pop(item_id)
     request.session['cart'] = cart
     messages.success(
                 request, f'Removed {service.name} from your cart')
     return redirect(reverse('view_cart'))
-
 
 
 def checkout(request):
