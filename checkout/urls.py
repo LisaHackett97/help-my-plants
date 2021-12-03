8f64f93d7e0d5ff69acc4f3357cb27ac65cfd5f4
LisaHackett97 <lisa-hackett@hotmail.com>
2021-12-03 10:13:48 +0000

    Add media query for cart sizing on small screens

--- checkout/urls.py
+++ checkout/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.checkout, name='checkout'),
+    path('checkout/', views.checkout, name='checkout'),
     path('cart/', views.view_cart, name='view_cart'),
     path('add/<item_id>', views.add_to_cart, name='add_to_cart'),
     path('remove/<item_id>/', views.remove_from_cart, name='remove_from_cart'),
