2a1e5d0cca31fdc0babab44648210b5be04f6206
LisaHackett97 <lisa-hackett@hotmail.com>
2021-12-04 12:45:16 +0000

    Register Article Model in admin

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from .models import Article
+
 # Register your models here.
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'content',
+        'image',
+        'created_date',
+        )
+
+admin.site.register(Article, ArticleAdmin)
