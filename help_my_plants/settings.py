9af50198aa42909d1be593c39957e34d33f6cef6
LisaHackett97 <lisa-hackett@hotmail.com>
2021-12-06 21:59:34 +0000

    Add stripe settings

--- help_my_plants/settings.py
+++ help_my_plants/settings.py
@@ -209,3 +209,10 @@ if 'USE_AWS' in os.environ:
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Stripe
+STRIPE_CURRENCY = 'eur'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+DEFAULT_FROM_EMAIL = 'helpmyplants@example.com'
