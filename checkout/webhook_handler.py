3e8f33d70093cf5973a4924cadf48015b17fb587
LisaHackett97 <lisa-hackett@hotmail.com>
2021-12-09 09:24:36 +0000

    Setup WH and test endpoint

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -3,14 +3,31 @@ from django.http import HttpResponse
 
 class StripeWH_Handler:
     """Handle Stripe Webhooks"""
-    
-    """ a setup method thats called every time an instance of the class is created """
+
     def __init__(self, request):
-        self.request - request
+        self.request = request
 
     def handle_event(self, event):
         """
         Handle a generic/unknown/unexpected webhook event
+        """
+        return HttpResponse(
+            content=f'Unhandled Webhook received: {event["type"]}',
+            status=200
+        )
+
+    def handle_payment_intent_succeeded(self, event):
+        """
+        Handle a payment intent succeded webhook frrom Stripe
+        """
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200
+        )
+
+    def handle_payment_intent_payment_failed(self, event):
+        """
+        Handle a payment_intent.payment_failed webhook frrom Stripe
         """
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
