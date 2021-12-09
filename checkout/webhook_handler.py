51623cc34bb118c3f270f91e1c42bfc2d58962ef
LisaHackett97 <lisa-hackett@hotmail.com>
2021-12-09 14:33:56 +0000

    Reverse change removing cart from WH Handler

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -26,7 +26,7 @@ class StripeWH_Handler:
         """
         intent = event.data.object
         pid = intent.id
-       
+        cart = intent.metadata.cart
         save_info = intent.metadata.save_info
         billing_details = intent.charges.data[0].billing_details
         order_total = round(intent.charges.data[0].amount / 100, 2)
