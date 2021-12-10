b4cdab83cb808d0408a1aed9714d09372af1f68b
LisaHackett97 <lisa-hackett@hotmail.com>
2021-12-10 18:23:02 +0000

    Remove cart app, not using

--- cart/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-
-@login_required
-def view_cart(request):
-    """ To render cart details """
-
-    return render(request, 'cart/cart.html')
