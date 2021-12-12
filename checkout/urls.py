194e5c2ad702f94508d87795bb821175888cd8fd
LisaHackett97 <lisa-hackett@hotmail.com>
2021-12-12 20:29:35 +0000

    Add checkout cahce

--- checkout/urls.py
+++ checkout/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('cart/', views.view_cart, name='view_cart'),
     path('add/<item_id>', views.add_to_cart, name='add_to_cart'),
     path('remove/<item_id>/', views.remove_from_cart, name='remove_from_cart'),
+    path('cache_checkout_data/', views.cache_checkout_data, name='cache_checkout_data'),
     path('wh/', webhook, name='webhook'),
 ]
