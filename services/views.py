2d30e047f95141244371a16762750e7a70769a9f
LisaHackett97 <lisa-hackett@hotmail.com>
2021-12-14 00:05:14 +0000

    Add Admin edit service template, url and view

--- services/views.py
+++ services/views.py
@@ -49,3 +49,27 @@ def add_service(request):
     }
 
     return render(request, template, context)
+
+
+def edit_service(request, service_id):
+    """ Edit a service in the store """
+    service = get_object_or_404(Service, pk=service_id)
+    if request.method == 'POST':
+        form = ServiceForm(request.POST, request.FILES, instance=service)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated service!')
+            return redirect(reverse('service_detail', args=[service.id]))
+        else:
+            messages.error(request, 'Failed to update service. Please ensure the form is valid.')
+    else:
+        form = ServiceForm(instance=service)
+        messages.info(request, f'You are editing {service.name}')
+
+    template = 'services/edit_service.html'
+    context = {
+        'form': form,
+        'service': service,
+    }
+
+    return render(request, template, context)
